858427776276b21bd29c5c0f8147430152c2bdd0
Wattier Julien <julienwattier@hotmail.fr>
2024-03-22 14:22:32 +0100

    feat: Random start, random victory flag, victory detection

--- app/src/main/java/helloandroid/ut3/minigame/services/GameService.java
+++ app/src/main/java/helloandroid/ut3/minigame/services/GameService.java
@@ -2,23 +2,34 @@ package helloandroid.ut3.minigame.services;
 
 import android.graphics.Bitmap;
 
+import java.util.Random;
+
 import helloandroid.ut3.minigame.data.Position;
 
 public class GameService {
     private static final int DEFAULT_RADIUS = 10;
     private static final Position DEFAULT_STARTING_POSITION = new Position(5 + DEFAULT_RADIUS, 5 + DEFAULT_RADIUS);
     private static final int DEFAULT_STEP = 1;
+
+    private static final int DEFAULT_BORDER_SIZE = 10;
+
+
     private static volatile GameService instance;
     private final int radius;
+    private final int victoryRadius;
     private final ImageService imageService = ImageService.getInstance();
 
     private final GyroscopeService gyroscopeService = GyroscopeService.getInstance();
     private Bitmap map;
     private Position currentPosition;
 
+    private Position victoryPosition;
+
+    private Position startingPosition;
+
     private GameService() {
         radius = DEFAULT_RADIUS;
-        currentPosition = DEFAULT_STARTING_POSITION;
+        victoryRadius = DEFAULT_RADIUS;
     }
 
     public static GameService getInstance() {
@@ -36,12 +47,20 @@ public class GameService {
         return radius;
     }
 
+    public int getVictoryRadius() {
+        return victoryRadius;
+    }
+
     public Bitmap getMap() {
         return map;
     }
 
     public Position getCurrentPosition() {
         return currentPosition;
+    }
+
+    public Position getVictoryPosition() {
+        return victoryPosition;
     }
 
     public void tick() {
@@ -78,7 +97,17 @@ public class GameService {
         currentPosition = nextPosition;
     }
 
+    public boolean isVictory() {
+        return victoryPosition.getY() == currentPosition.getY() && victoryPosition.getX() == victoryPosition.getX();
+    }
+
     public void setup() {
         map = imageService.getThresholdedBitmap();
+        Random r = new Random();
+        //Starting position
+        startingPosition = new Position(r.nextInt(map.getHeight() - 2 * DEFAULT_BORDER_SIZE + DEFAULT_BORDER_SIZE), r.nextInt(map.getHeight() - 2 * DEFAULT_BORDER_SIZE + DEFAULT_BORDER_SIZE));
+        victoryPosition = new Position(r.nextInt(map.getHeight() - 2 * DEFAULT_BORDER_SIZE + DEFAULT_BORDER_SIZE), r.nextInt(map.getHeight() - 2 * DEFAULT_BORDER_SIZE + DEFAULT_BORDER_SIZE));
+
+        currentPosition = startingPosition;
     }
 }
